a19c059749d1acc27e7de400f370df6aa970827a
control-alt-f4 <109923311+CardboardDog@users.noreply.github.com>
2023-08-05 15:43:40 -0400

    +love2d folder

--- test.bat
+++ test.bat
@@ -1,2 +1,5 @@
-.\\make.bat
+pyinstaller ./editor/main.py --onefile
+powershell -command "Compress-Archive .\love2d\ .\dist\love.zip -Update"
+copy /b .\\dist\\main.exe + .\\dist\\love.zip .\\dist\\release\\Bit7.exe
+copy .\\love2d\\license.txt .\\dist\\release\\license.txt
 .\\dist\\release\\Bit7.exe
